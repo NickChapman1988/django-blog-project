c7fcbc1a00246cdcc2dc67225adaa231138ee7d7
NickChapman1988 <muppetnj_uk@hotmail.com>
2023-01-20 11:07:59 +0000

    Add temporary blog posts to views.py and business.html

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -6,5 +6,24 @@ def index(request):
     return render(request, 'blog/home.html')
 
 
+posts = [
+    {
+        'author': 'Loonycorn',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'October 25, 2019'
+    },
+    {
+        'author': 'Test',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'October 25, 2019'
+    }
+]
+
+
 def business(request):
-    return render(request, 'blog/business.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/business.html', context)
