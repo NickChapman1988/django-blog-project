a0c2281ec215525547341f1b136b078524feb4b4
NickChapman1988 <muppetnj_uk@hotmail.com>
2023-01-24 17:02:40 +0000

    create users app

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,29 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 
 def index(request):
     return render(request, 'blog/home.html')
 
 
-posts = [
-    {
-        'author': 'Loonycon',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'October 25, 2019'
-    },
-    {
-        'author': 'Test',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'October 25, 2019'
-    }
-]
-
-
 def business(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/business.html', context)
