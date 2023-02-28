bab01242b83997a93d0bfb906bcbd2f0138f915c
NickChapman1988 <muppetnj_uk@hotmail.com>
2023-02-28 14:42:28 +0000

    Create PostListView in views.py

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView
 
 
 def index(request):
@@ -12,3 +13,9 @@ def business(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/business.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/business.html'
+    context_object_name = 'posts'
